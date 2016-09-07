9cdaa1e18f40e4892d8f0f396f3dbf0352237961
Yaron Eidelman <dogkiller-e@hotmail.com>
2016-09-07 07:37:59 -0400

    Added git configuration to save username and password localy

--- git.sh
+++ git.sh
@@ -6,7 +6,14 @@ echo "Please entert email associated with your git acount:"
 read email
  
 echo -n "Please enter your name:"
+
 read  fname lname
+
+# Set git to use the credential memory cache
+git config --global credential.helper cache
+
+git config --global credential.helper 'cache --timeout=3600'
+# Set the cache to timeout after 1 hour (setting is in seconds)
 
 git config --global --replace-all user.name "$fname $lname"
 git config --global user.email $email
