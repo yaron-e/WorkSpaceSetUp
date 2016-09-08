747b7faebab3a3494e04b4c7b95f686031fc678d
Yaron Eidelman <dogkiller-e@hotmail.com>
2016-09-08 08:59:50 -0400

    added nodejs and npm installation and set up

---
+++ nodejsSetUp.sh
@@ -0,0 +1,14 @@
+npm init
+npm install express -save
+
+#word ="var express = require('express');"
+#word +="var app = express();"
+#word +="var port = process.env.port || 3000;"
+#word +="app.get('/', function (req, res) {"
+#word +="res.send('Hello World!');"
+#word +="});"
+#word +="app.listen(port, function () {"
+#word +="console.log('Example app listening on port’ + port);"
+#word +="});"
+
+#echo $word
