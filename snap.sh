ec972c653d15e9ccd5cab47b2857a86c89da0c31
yaron-e <yaron-e@users.noreply.github.com>
2018-07-13 10:59:57 -0400

    Created install script using Snap
    Created a script which will install everything I need into a new workspace using snap tool.

---
+++ snap.sh
@@ -0,0 +1,11 @@
+sudo snap install spotify
+sudo snap install eclipse --channel 
+sudo snap install android-studio --channel 
+sudo snap install pycharm-community --channel 
+sudo snap install intellij-idea-community --channel 
+sudo snap install rubymine --channel 
+sudo snap install notepadqq --channel  
+sudo snap install skype --channel 
+sudo snap install vlc --channel 
+sudo snap install chromium --channel 
+sudo snap install simplenote-jamiebennett --channel 
